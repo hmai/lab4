33677dc07505cf540e6d948852fffa051bd806ed
hoa mai <hmai_23@yahoo.com>
2016-01-28 20:15:14 +0000

    Supporting stretch goals

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,12 @@
+
+
 exports.viewProject = function(req, res) { 
   // controller code goes here 
   var name = req.params.name; 
   console.log("The project name is: " + name);
+  
   res.render('project', {
-  	'projectName': name
+  	'jtitle': name,
+  	'jdescription': "one-sentence description of project"
   });
 };
